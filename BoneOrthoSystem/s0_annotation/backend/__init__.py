71d79ade2953ef00616b2ed0ec326632d0068993
fjufju152-netizen <fjufju152@gmail.com>
2025-12-12 09:25:28 +0800

    虛擬環境/
    Co-Authored-By: CHEN YI SHAN <165142732+Shan11111111@users.noreply.github.com>

--- BoneOrthoSystem/s0_annotation/backend/__init__.py
+++ BoneOrthoSystem/s0_annotation/backend/__init__.py
@@ -1,4 +1,4 @@
-# BoneOrthoBackend/s0_annotation/__init__.py
+# s0_annotation/backend__init__.py
 from fastapi import APIRouter
 from .annotations import router as annotations_router
 from .big_bones import router as big_bones_router
